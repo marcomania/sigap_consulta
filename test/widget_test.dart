711efe960cd6337c6e0e73741b6deb2f1dada7df
Marco Untiveros <marcow85m@gmail.com>
2020-09-11 20:01:33 -0500

    funciones de test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sigap_consulta/main.dart';
+import 'package:fisi_army/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
